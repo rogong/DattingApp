c2a8b4e4fb7477b9ee1c0806c11b7f7220d2d1d6
rogong <itingasng@gmail.com>
2019-02-15 14:51:48 +0100

    Handling Exception Added

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
       .subscribe(next => {
         console.log('Logged in succesfully');
       }, error => {
-        console.log('Failed to login');
+        console.error('error');
       });
   }
 
